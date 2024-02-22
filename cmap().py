488c02f1445d6abf2e8dfd51783076917e79031f
Johann J Cardenas <johannc2@illinois.edu>
2024-02-22 00:32:21 -0600

    Update figure size and dpi in cmap().py

--- cmap().py
+++ cmap().py
@@ -1,4 +1,5 @@
 # Generate default colormaps available in Matplotlib
+# Suitable for countour plots, heatmaps, etc.
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ cmaps = plt.colormaps()
 cmaps.sort()
 
 # Creating a figure to display examples of colormaps
-fig, axes = plt.subplots(nrows=len(cmaps)//10, ncols=10, figsize=(30, 32))
+fig, axes = plt.subplots(nrows=len(cmaps)//10, ncols=10, figsize=(30, 32), dpi=300)
 fig.subplots_adjust(top=0.98, bottom=0.02, left=0.2, right=0.99, hspace=0.3, wspace=0.1)
 
 for ax, cmap in zip(axes.flatten(), cmaps):
